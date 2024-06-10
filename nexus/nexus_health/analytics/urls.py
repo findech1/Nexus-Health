f57005c2de68d2bbec0be951d1f41b898591ebd5
ndege felix <findech6@gmail.com>
2024-06-10 22:11:12 +0300

    upadated navigation,urls,backend

--- nexus/nexus_health/analytics/urls.py
+++ nexus/nexus_health/analytics/urls.py
@@ -1,10 +1,4 @@
-# from django.urls import path
-# from .views import AnalyticsRecordListView, AnalyticsRecordDetailView
 
-# urlpatterns = [
-#     path('', AnalyticsRecordListView.as_view(), name='analytics-record-list'),
-#     path('<int:pk>/', AnalyticsRecordDetailView.as_view(), name='analytics-record-detail'),
-# ]
 from django.urls import path
 from .views import AnalyticsDashboardView, AnalyticsReportView
 
--- nexus/nexus_health/ehr/urls.py
+++ nexus/nexus_health/ehr/urls.py
@@ -1,10 +1,4 @@
-# from django.urls import path
-# from .views import HealthRecordListView, HealthRecordDetailView
 
-# urlpatterns = [
-#     path('', HealthRecordListView.as_view(), name='health-record-list'),
-#     path('<int:pk>/', HealthRecordDetailView.as_view(), name='health-record-detail'),
-# ]
 from django.urls import path
 from .views import EHRListView, EHRDetailView
 
